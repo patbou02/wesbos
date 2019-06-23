16ad511cbad514a17da9a92a935c8c2c1a5f2922
Patrick Boutin <patrick.boutin@roberthalf.com>
2019-06-23 00:02:45 -0700

    Add starting HTML, CSS and WAV files for Drum Kit exercise.

---
+++ shared/reload.js
@@ -0,0 +1,5 @@
+"use strict";
+
+console.info('LiveReload enabled. Watching...');
+
+document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>');
