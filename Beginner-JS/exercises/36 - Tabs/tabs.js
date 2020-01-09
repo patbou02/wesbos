0bdfb762f51d033f14f2b1f6a8d62b8fd104b1f1
Patrick Boutin <patrick.boutin@roberthalf.com>
2020-01-08 22:15:08 -0800

    Initial answer without looking at video tutorial answer.

--- Beginner-JS/exercises/36 - Tabs/tabs.js
+++ Beginner-JS/exercises/36 - Tabs/tabs.js
@@ -1,1 +1,34 @@
 console.log('ya ya wes we get it.. IT WORKS!');
+
+// Grab page elements
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
+
+
+
+tabButtons.forEach(function(element) {
+  element.addEventListener('click', handleTabClick);
+});
+
+function handleTabClick(event) {
+  // hide all tab panels
+  //console.info(tabPanels);
+  tabPanels.forEach(panel => {
+    panel.hidden = true;
+    //console.info(panel.attributes['aria-labelledby'].value);
+  });
+  // mark all tabs as unselected
+  tabButtons.forEach(function(tab) {
+    tab.attributes['aria-selected'].value = 'false';
+  });
+  // mark the clicked tab as selected
+  event.originalTarget.attributes['aria-selected'].value = 'true';
+  // find the associated tabPanel and show it.
+  const tabTopic = event.originalTarget.attributes['id'].value;
+  tabPanels.forEach(function(panel) {
+    if (tabTopic === panel.attributes['aria-labelledby'].value) {
+      panel.hidden = false;
+    }
+  });
+}
