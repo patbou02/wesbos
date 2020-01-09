7c118065adce7ddd49e9b7f70cf53f3548b64812
Patrick Boutin <patrick.boutin@roberthalf.com>
2020-01-08 23:21:07 -0800

    Refactor to use method #2 which uses Array.find() method and sets hidden to false when aria attribute is same as ID.

--- Beginner-JS/exercises/36 - Tabs/tabs.js
+++ Beginner-JS/exercises/36 - Tabs/tabs.js
@@ -3,7 +3,8 @@ console.log('ya ya wes we get it.. IT WORKS!');
 // Grab page elements
 const tabs = document.querySelector('.tabs');
 const tabButtons = tabs.querySelectorAll('[role="tab"]');
-const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
+// convert to array so that Array.find() method works
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
 
 function handleTabClick(event) {
   // hide all tab panels
@@ -16,10 +17,21 @@ function handleTabClick(event) {
   // mark the clicked tab as selected
   event.currentTarget.setAttribute('aria-selected', true);
 
-  // find the associated tabPanel and show it
   const { id } = event.currentTarget;
+  /*
+  Method #1
+  // find the associated tabPanel and show it
   const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
   tabPanel.hidden = false;
+   */
+
+  // Method #2
+  //console.log(tabPanels);
+  tabPanels.find(function(panel) {
+    if (panel.getAttribute('aria-labelledby') === id) {
+      panel.hidden = false;
+    }
+  });
 }
 
 tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
